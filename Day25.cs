dbad154c73e40c9138ea02b46303cefdd39663de
Albinosten <abbeohebbe@live.se>
2025-01-14 21:06:44 +0100

    Final day. all puzzles + examples run in 4m10sek

---
+++ Day25.cs
@@ -0,0 +1,85 @@
+﻿namespace AdventOfCode2024
+{
+	
+	internal class Day25 : IPuzzle
+	{
+		public bool IsExample { get; set; }
+		public int FirstResult => this.IsExample ? 3 : 3090;
+
+		public long SecondResult => this.IsExample ? 0 : 0;
+
+		public string path => this.IsExample
+			? $"Inputs/{this.GetType().Name}_TEMP.txt"
+			: $"Inputs/{this.GetType().Name}.txt";
+		public int First()
+		{
+			var (locks,keys) = ParseInput();
+			var result = 0;
+			for(int i = 0; i < locks.Count; i++)
+			{
+				for (int j = 0; j < keys.Count; j++)
+				{
+					var isMatch = true;
+					for (int pin = 0; pin < 5; pin++)
+					{
+						if (locks[i][pin] + keys[j][pin] > 5)
+						{
+							isMatch = false;
+						}
+					}
+					if (isMatch)
+					{
+						result++;
+					}
+				}
+			}
+
+			return result;
+		}
+
+		public long Second()
+		{
+			return 0;
+		}
+
+		private (List<List<int>>locks, List<List<int>> keys) ParseInput()
+		{
+			var allLines = File.ReadAllLines(this.path).Chunk(8);
+			var locks = new List<List<int>>();
+			var keys = new List<List<int>>();
+
+			foreach (var line in allLines)
+			{
+				var _lock = new List<int>();
+				var key = new List<int>();
+				var isLock = line[0][0] == '#';
+				for (int i = 0; i < 5; i++)
+				{
+					var number = line
+						.Where(x => !string.IsNullOrEmpty(x))
+						.Select(x => x[i])
+						.Count(x => x == '#')
+						-1;
+					if(isLock)
+					{
+						_lock.Add(number);
+					}
+					else
+					{
+						key.Add(number);
+					}
+				}
+				if (isLock)
+				{
+					locks.Add(_lock);
+				}
+				else
+				{
+					keys.Add(key);
+				}
+			}
+
+			return (locks,keys);
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,30 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AdventOfCode2024;
 using System.Diagnostics;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Windows.Input;
 
-Console.WriteLine("Hello, World!");
-
-//var puzzles = AppDomain
-//	.CurrentDomain
-//	.GetAssemblies()
-//	.SelectMany(x => x.GetTypes())
-//	.Where(x => typeof(IPuzzle).IsAssignableFrom(x))
-//	.Where(x => x.IsClass)
-//	.Select(x => Create(x))
-//	.ToList();
 var watch = new Stopwatch();
 watch.Start();
 
-//var a = new Day20()
-//{
-//	IsExample = true
-//};
-//Console.WriteLine(a.First());
-//Console.WriteLine(a.Second());
-
+Print(new Day25());
 Print(new Day24());
 Print(new Day23());
 Print(new Day22());
@@ -87,7 +68,6 @@ static void ThrowIfNotSame<T>(T actual, T expected, string puzzleName)
 {
 	if (!actual.Equals(expected))
 	{
-		//Console.Clear();
 		throw new Exception("Expected value: " + expected + " Actual value: " + actual + " On day: " + puzzleName);
 	}
 }
